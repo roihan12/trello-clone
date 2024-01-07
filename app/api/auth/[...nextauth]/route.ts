849319be3f6694ed6e7fc6f3bf6836af9136da6f
Your Name <roihansori12@gmail.com>
2024-01-07 15:13:18 +0700

    fix: nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-
-export const authOptions = {
+import GoogleProvider from "next-auth/providers/google";
+import NextAuth, { AuthOptions } from "next-auth";
+export const authOptions: AuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -10,7 +9,8 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-}
+};
 
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST}
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
